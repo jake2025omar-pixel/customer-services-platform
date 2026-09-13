25d13571dd17c9c693c338b7e1e0f6dd384ea1f7
jake <jake2025omar@gmail.com>
2026-09-13 18:43:50 +0300

    fix pages

--- vite.config.ts
+++ vite.config.ts
@@ -153,6 +153,7 @@ function vitePluginManusDebugCollector(): Plugin {
 const plugins = [react(), tailwindcss(), jsxLocPlugin(), vitePluginManusRuntime(), vitePluginManusDebugCollector()];
 
 export default defineConfig({
+ base: "/customer-services-platform/",
   plugins,
   resolve: {
     alias: {
